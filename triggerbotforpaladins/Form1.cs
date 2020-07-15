641722ab6fbd719a88ef29ec45234f0f4ab5bafb
Swedish Twat <kalliokoski.elias@gmail.com>
2020-07-15 19:20:00 +0200

    Fixed a thing

--- triggerbotforpaladins/Form1.cs
+++ triggerbotforpaladins/Form1.cs
@@ -1,30 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using AutoItX3Lib;
+
 
 namespace triggerbotforpaladins
 {
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll")] // I love getasynckeystate
         static extern short GetAsyncKeyState(Keys vkey);
 
 
         object pixel;
 
         int newCol;
+        int[] coor = { 961, 527 }; // works for 1920x1080
         
-        
-        AutoItX3 au3 = new AutoItX3();
+        AutoItX3 au3 = new AutoItX3(); 
 
         public Form1()
         {
@@ -33,7 +27,7 @@ namespace triggerbotforpaladins
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Thread mn = new Thread(Main);
+            Thread mn = new Thread(Main) { IsBackground = true }; // You can do this with a backgroundworker if you want, I prefer threads tho
             mn.Start();
         }
 
@@ -41,13 +35,13 @@ namespace triggerbotforpaladins
         {
             while (true)
             {
-                if (GetAsyncKeyState(Keys.XButton2)<0)
+                if (GetAsyncKeyState(Keys.XButton2)<0) // Change the hotkey to whatever you want, mouse5 is just my fav.
                 {
                     newCol = GrabColor();
 
-                    if (newCol > 7000000)
+                    if (newCol > 7000000) // Crosshair color won't go over this value, it's changing between 4000000 and 6500000. When aiming at the enemy the red and orange color is over 10000000
                     {
-                        au3.MouseClick("LEFT");
+                        au3.MouseClick("LEFT"); // I got problems with mouse_events in this game so just made everything in autoitx 
                         Thread.Sleep(10);
                     }
                     Thread.Sleep(10);
@@ -58,7 +52,7 @@ namespace triggerbotforpaladins
 
         int GrabColor()
         {
-            pixel = au3.PixelGetColor(961, 527);
+            pixel = au3.PixelGetColor(coor[0], coor[1]); // These coordinates work for 1920x1080, make sure to check your own resolution in-game and set your own if you don't have the same resolution as me.
             return Int32.Parse(pixel.ToString());
         }
     }
